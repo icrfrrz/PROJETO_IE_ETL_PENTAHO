9db3689ea75bf481222a2b2fb257a1d6db3f8161
icrfrrz <icaro.ferraz@live.com>
2025-02-04 17:03:14 -0300

    python e excel

--- SCRIPTS/WEBSCRAPING.py
+++ SCRIPTS/WEBSCRAPING.py
@@ -1,25 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-# URL do produto no Mercado Livre
-url = "https://www.mercadolivre.com.br/apple-iphone-13-128-gb-estelar-distribuidor-autorizado/p/MLB1018500855#polycard_client=search-nordic&searchVariation=MLB1018500855&wid=MLB3582540395&position=1&search_layout=stack&type=product&tracking_id=f78bef3b-d483-414f-b0d2-83b87ca5ccce&sid=search"
+# Configurar pandas para exibir todas as linhas
+pd.set_option('display.max_rows', None)
 
-# Enviar uma requisição GET para a URL
-response = requests.get(url)
+df = pd.read_excel('D:\dataset_administradora.xlsx')
 
-# Verificar se a requisição foi bem-sucedida
-if response.status_code == 200:
-    # Parsear o conteúdo HTML da página
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    # Encontrar o elemento que contém o preço
-    price_element = soup.find_all('span', {'class': 'andes-money-amount__fraction'})
-    
-    if price_element:
-        # Extrair o texto do elemento
-        price = price_element[2].text
-        print(f"O preço atual do produto é: R$ {price}")
-    else:
-        print("Não foi possível encontrar o preço do produto.")
-else:
-    print(f"Falha ao acessar a página. Status code: {response.status_code}")
+# Filtrar os dados onde nm_administradora começa com "AFFIX"
+filtered_df = df[df['contrato.nm_administradora'].str.startswith('AFFIX')]
+
+# Exibir todas as colunas e linhas do DataFrame filtrado
+print(filtered_df)
